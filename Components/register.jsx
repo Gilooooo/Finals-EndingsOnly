20f5753e29a82947bd86a35b0e05e8f83781b540
Gilooooo <118784503+Gilooooo@users.noreply.github.com>
2023-01-11 13:54:58 +0800

    Add files via upload

---
+++ Components/register.jsx
@@ -0,0 +1,16 @@
+import styles from "./register.module.css";
+
+export default function(){
+    return(
+        <div>
+            <div>
+                <div>
+                    <input type="text" className="Firstname" placeholder="FIRSTNAME"/>
+                </div>
+            </div>
+            <div>
+                <img src="image-removebg-preview (1) 2.png"></img>
+            </div>
+        </div>
+    )
+}
