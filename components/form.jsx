1baa448319da0a27db5373518c9b0303586c0565
Gilooooo <118784503+Gilooooo@users.noreply.github.com>
2023-01-18 16:57:29 +0800

    Update form.jsx

--- components/form.jsx
+++ components/form.jsx
@@ -1,11 +1,11 @@
-import styles from "../components/form.module.css";
+import styles from "../components/Form.module.css";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import React, { useState } from "react";
 import Axios from "axios";
 
-export default function FORM(){
+export default function FORM (){
     const [firstName, setfirstName] = useState("")
     const [LastName, setLastName] = useState("")
     const [Email, setEmail] = useState("")
@@ -21,8 +21,8 @@ export default function FORM(){
         Email: yup.string().email().required(),
         Mobile: yup.string().matches(phoneRegExp, 'Phone number is not valid'),
         Address: yup.string().required(),
-        old_stud: yup.string().required(),
-        Reason: yup.string().required("Are you old student or not?"),
+        old_stud: yup.string().required("Are you old student or not?"),
+        Reason: yup.string().required(),
     });
     
     const{register, handleSubmit, formState:{errors} } = useForm({
@@ -45,65 +45,95 @@ export default function FORM(){
     }; 
 
     return(
-        <div>
-            <div className={styles.form}>
-                <h1>REGISTRATION FORM</h1>
+        <div  className={styles.main}>
+            <div className={styles.register}>
+                <h1>Registration Form</h1>
             </div>
-                <div onSubmit={handleSubmit(submitForm)} className={styles.container1}>          
-                        <div  className={styles.container2}>
-                            <form >
-                                <h3>FIRSTNAME</h3>
-                                    <input type="text"  className={styles.input} {...register("firstName")} 
-                                    onChange={(e) => {setfirstName(e.target.value)}}/><br /><br />
-                                        <p>{errors.firstName?.message}</p><br />
-
-                                <h3>EMAIL</h3>
-                                    <input type="text"  className={styles.input} {...register("Email")}
-                                    onChange={(e) =>{setEmail(e.target.value)}}/><br /><br />
-                                        <p>{errors.Email?.message}</p><br />
-
-                                <h3>ADDRESS</h3>
-                                    <input type="text"  className={styles.input} {...register("Address")}
-                                    onChange={(e) => {setAddress(e.target.value)}}/><br /><br />
-                                        <p>{errors.Address?.message}</p><br />
-                            </form>
-                            <form >
-                                <h3>LASTNAME</h3>
-                                    <input type="text"  className={styles.input} {...register("LastName")}
-                                    onChange={(e) =>{setLastName(e.target.value)}}/><br /><br />
-                                        <p>{errors.LastName?.message}</p><br />
-
-                                <h3>MOBILE NUMBER</h3>
-                                    <input type="number"  className={styles.input} {...register("Mobile")}
-                                    onChange={(e)=>{setMobile(e.target.value)}}/><br /><br />
-                                        <p>{errors.Mobile?.message}</p><br />
-
-                                <div className={styles.option}>
-                                    <h3>OLD TUP STUDENT?</h3><br />
-                                    <div className={styles.option1}>
-                                        <label htmlfor="yes" >YES</label>
-                                            <input type="radio" name="Option" value="yes" className={styles.radio} {...register("old_stud")}
-                                            onChange={(e) =>{setold_stud(e.target.value)}}/>
-                                        <label htmlfor="no" >NO</label>
-                                            <input type="radio" name="Option" value="no" className={styles.radio} {...register("old_stud")}
-                                            onChange={(e) =>{setold_stud(e.target.value)}} /><br />
-                                                
-                                    </div>
-                                        <p>{errors.old_stud?.message}</p><br />
-                                </div>    
-                                        <input type="text" placeholder="Why do you want to study here?" className={styles.reason} {...register("Reason")}
-                                        onChange={(e) =>{setReason(e.target.value)}}/><br />  
-                                            <p>{errors.Reason?.message}</p><br />     
-                                                <button >SUBMIT</button>
-                                        
-                            </form>
+                <div onSubmit={handleSubmit(submitForm)} className={styles.container1}>
+                    <form  className={styles.container2}>
+                        <div className={styles.names}>
+                            <div>
+                                <h3>FIRST NAME</h3>
+                                <input type="text" 
+                                    {...register("firstName")} 
+                                    onChange={(e) => {setfirstName(e.target.value)}}
+                                    />
+                                    <p>{errors.firstName?.message}</p><br />
+                            </div>
+                            <div>
+                                <h3>LAST NAME</h3>
+                                <input type="text" 
+                                    {...register("LastName")}
+                                    onChange={(e) =>{setLastName(e.target.value)}}
+                                    /> 
+                                    <p>{errors.LastName?.message}</p><br />
+                            </div>            
                         </div>
-                    <div className={styles.image}>
+                        <div className={styles.enumber}>
+                            <div>
+                                <h3>EMAIL</h3>
+                                <input type="text" 
+                                    {...register("Email")}
+                                    onChange={(e) =>{setEmail(e.target.value)}}
+                                    />
+                                    <p>{errors.Email?.message}</p><br />
+                            </div>
+                            <div>
+                                <h3>MOBILE NUMBER</h3>
+                                <input type="number" 
+                                    {...register("Mobile")}
+                                    onChange={(e)=>{setMobile(e.target.value)}}
+                                    />
+                                    <p>{errors.Mobile?.message}</p><br />  
+                            </div>      
+                        </div>
+                        <div className={styles.oldaddress}>      
+                            <div className={styles.address}>
+                                <h3>ADDRESS</h3>
+                                    <input type="text" 
+                                        {...register("Address")}
+                                        onChange={(e) => {setAddress(e.target.value)}}
+                                        />
+                                        <p>{errors.Address?.message}</p><br />
+                                        
+                            </div>                               
+                                <div className={styles.old}>
+                                    <h3 className={styles.olds}>OLD TUP STUDENT?</h3>
+                                        <div className={styles.options}>
+                                            
+                                            <label htmlFor="Yes">Yes</label>
+                                            <input type="radio" name="option" value="Yes"  
+                                                {...register("old_stud")}
+                                                onChange={(e) =>{setold_stud(e.target.value)}}
+                                                />
+                                        <label htmlFor="No">No</label>
+                                            <input type="radio" name="option" value="No"
+                                                {...register("old_stud")}
+                                                onChange={(e) =>{setold_stud(e.target.value)}}
+                                                />  
+                                        </div>  
+                                        <p>{errors.old_stud?.message}</p><br />                                
+                                </div>
+                                
+                        </div>
+                        <div className={styles.reason}>
+                            <input type="text" placeholder="Why do you want to study here?" 
+                                {...register("Reason")}
+                                onChange={(e) =>{setReason(e.target.value)}}
+                                />
+                                
+                                    
+                        </div>
+                        <p>{errors.Reason?.message}</p><br /> 
+                                    <button>SUBMIT</button>
+                        <div>
+                            
+                        </div>
+                    </form>
+                    <div>        
                         <img src="image-removebg-preview (1) 2.png" />
-                    </div>  
-                
-                </div>
-                    
+                    </div>   
+            </div>      
         </div>
-    )   
+    )
 }
